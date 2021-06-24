76bb3b6e298097661f502172a4f845c2f55f69b3
Ritwik Bahukhandi <ritwikbahukhandi98@gmail.com>
2021-06-24 14:51:34 +0530

    Streamlit File Updated
    Add some features to the processing page

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,4 @@
+  
 import streamlit as st
 import joblib
 import os
@@ -43,19 +44,19 @@ def get_clean(x):
 
 if choice == "Prediction":
 	heading = st.title("Fake News Classifier")
-	note = st.info("Note : This app will supposed to do prediction on the basis of Article's Features (TF-IDF) using SGD CLassifer!!")
+	note = st.write("Note : This app will supposed to do prediction on the basis of Article's Features (TF-IDF) using SGD CLassifer!!")
 	article = st.text_area('Enter the News Article')
 	submit_button = st.button('Predict')
 
 	
 	if submit_button and len(article)==0 :
-		st.info("Sorry , You haven't entered any article yet !!!!!")
+		st.markdown("Sorry , You haven't entered any article yet !!!!!")
 		
 
 
 		
 	if submit_button and len(article) != 0 :
-		st.info( article)
+		
 		#INPUT TEXT PREPROCESSING
 		article_processed =get_clean(article)
 
@@ -69,10 +70,9 @@ if choice == "Prediction":
 		vector = np.resize(vector, (1, 106186))
 
 		
-		st.write("Prediction :")
 
 		prediction = clf.predict(vector)
-		st.info(str(prediction))
+		st.write("Prediction : " , str(prediction))
 		
 
 
@@ -82,13 +82,14 @@ if choice == "Prediction":
 #confusion matrix
 
 if choice == "Confusion Matrix":
-	st.title("Confusion matrix")
-	def img_to_bytes(img_path):
-		img_bytes = Path(img_path).read_bytes()
-		encoded = base64.b64encode(img_bytes).decode()
-		return encoded
-	image1 = img_to_bytes("SGD.png")
-	st.image(image1)
+	# st.title("Confusion matrix")
+	# def img_to_bytes(img_path):
+	# 	img_bytes = Path(img_path).read_bytes()
+	# 	encoded = base64.b64encode(img_bytes).decode()
+	# 	return encoded
+	# image1 = img_to_bytes("SGD.png")
+	# st.image(image1)
+	pass
     
 
 if choice == "Text Preprocessing":
@@ -98,33 +99,26 @@ if choice == "Text Preprocessing":
 	select_processing = ["Process" , "Tokenize" , "Vectorizer"]
 	choice1 = st.selectbox("Choose" ,select_processing )
 	
-
-	if choice1 == "Process" and st.button('Submit') :
+	submit_button = st.button("Submit")
+	if choice1 == "Process" and submit_button :
 		article_processed = get_clean(article)
-		st.info(article_processed)
+		st.markdown( article_processed)
 
-	if  choice1 == "Tokenize" and st.button('Submit') :
+	if  choice1 == "Tokenize" and submit_button :
 		article_processed = get_clean(article)
 		tokens = word_tokenize(article_processed)
-		st.info(tokens)
+		st.markdown(tokens)
 
-	if choice1 == "Vectorizer" and st.button('Submit'):
+	if choice1 == "Vectorizer" and submit_button:
 		article_processed = get_clean(article)
 		tokens = word_tokenize(article_processed)
 		tfidf = TfidfVectorizer()
 		vector = tfidf.fit_transform(tokens)
-		st.info(vector)
+		st.markdown(vector)
 
-
-
-		
-
-
-	
-	
-
-
-
-
-
+	for i in range(len(select_processing)):
+		if submit_button and len(article) == 0 :
+			if (select_processing[i]):
+				st.write("You haven't entered any text yet!!")
+				break
 
